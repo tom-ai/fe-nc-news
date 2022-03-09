06c73750d8d449131452ea687a77e1bf74ca9577
Tom Harris <tomharris3@me.com>
2022-03-09 17:57:54 +0000

    article page displays article

--- src/components/ArticlePage.jsx
+++ src/components/ArticlePage.jsx
@@ -13,7 +13,7 @@ function ArticlePage() {
 
   const { article_id: id } = useParams();
 
-  const [article, setArticle] = useState();
+  const [article, setArticle] = useState([]);
 
   useEffect(() => {
     api.getArticleById(id).then((article) => {
@@ -23,7 +23,17 @@ function ArticlePage() {
 
   return (
     <>
-      <Article articleId={article} />
+      <Article
+        key={article.article_id}
+        articleId={article.article_id}
+        body={article.body}
+        commentCount={article.comment_count}
+        username={article.author}
+        timestamp={article.created_at}
+        title={article.title}
+        topic={article.topic}
+        votes={article.votes}
+      />
       {/* <CommentList /> */}
     </>
   );
