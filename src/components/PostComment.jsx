7de81e2e9c10eb1ea7d104a70180059af351d97e
Tom Harris <tomharris3@me.com>
2022-03-20 15:19:55 +0000

    comments can be submitted with enter key

--- src/components/PostComment.jsx
+++ src/components/PostComment.jsx
@@ -1,4 +1,4 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import * as api from "../utils/api";
 
 function PostComment({ articleId, users }) {
@@ -23,13 +23,14 @@ function PostComment({ articleId, users }) {
       // do something here like isLoading
     } else {
       e.preventDefault();
+      setFormData(initialFormData);
       api.postComment(articleId, formData);
     }
   };
 
   return (
     <>
-      <form>
+      <form onSubmit={handleSubmit}>
         <div>
           <div className="flex items-end mb2">
             <label htmlFor="comment" className="f5 f4-l b db mr2">
@@ -53,16 +54,18 @@ function PostComment({ articleId, users }) {
             </select>
           </div>
 
-          <textarea
+          <input
+            defaultValue={""}
             onChange={handleChange}
             id="body"
             name="body"
             className="f4 f3-l db border-box hover-black w-100 measure ba b--black-20 pa2 br2 mb2"
-          ></textarea>
+          ></input>
         </div>
       </form>
 
       <button
+        type="submit"
         onClick={handleSubmit}
         className="f5 f4-l grow no-underline br-pill ba bw1 ph3 pv2 mb3 b--dark-red dark-red bg-light-yellow"
       >
